5691c8acdc96a9b03b51a39353bf1d829da8ce43
Bruno Gama <brrgama@gmail.com>
2021-11-08 11:31:22 -0300

    SSH communication - It was created a method to execute a command in a remote device using SSH connection.

---
+++ CSharpStudy/GeneralLibrary/SshCommunication.cs
@@ -0,0 +1,80 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralLibrary
+{
+    public class SshCommunication
+    {
+        /// <summary>
+        /// Ssh server IP.
+        /// </summary>
+        public string Ip { get; set; }
+
+        /// <summary>
+        /// Ssh server user name.
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Ssh server password.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Ssh port.
+        /// </summary>
+        public const int Port = 22;
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        public SshCommunication(IPAddress ip, string user, string password)
+        {
+            Ip = ip.MapToIPv4().ToString();
+            User = user;
+            Password = password;
+        }
+
+        /// <summary>
+        /// Connect to a remote device using ssh connection and execute a command in this device. Returns the device response.
+        /// </summary>
+        /// <param name="command"> The command to be executed.</param>        
+        /// <param name="timeout"> Timeout to cancel the command execution.</param>    
+        /// <param name="cmdReturn"> The device return.</param>        
+        private bool ExecuteCommand(string command, int timeout, out string cmdReturn)
+        {
+            bool control = false;
+            cmdReturn = "";
+            try
+            {
+                using (var ssh = new SshClient(Ip, Port, User, Password))
+                {
+                    ssh.Connect();
+
+                    using (var cmd = ssh.CreateCommand(command))
+                    {
+                        // Define timeout for the command execution.
+                        cmd.CommandTimeout = TimeSpan.FromSeconds(timeout);
+
+                        cmdReturn = cmd.Execute(command);
+                    }
+                    ssh.Disconnect();
+                    control = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                control = false;
+            }
+            return control;
+        }
+    }
+}
---
+++ CSharpStudy/GeneralLibrary/TasksManagement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeneralLibrary
+{
+    public class TasksManagement
+    {
+        private bool AnyMethod()
+        {
+            // Any long operation.
+            return false;
+        }
+
+        public void TaskCancellation()
+        {
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = tokenSource.Token;
+            int timeout = 30;
+            bool taskReturn;
+            
+            var task = Task.Run( () => AnyMethod(), cancellationToken);
+
+            if (task.Wait(TimeSpan.FromSeconds(timeout)))
+            {
+                taskReturn = task.Result; 
+            }
+            else
+            {
+
+                tokenSource.Cancel(); // This will not cancel the task.
+                throw new Exception("Time out");
+            }
+        }
+    }
+}
